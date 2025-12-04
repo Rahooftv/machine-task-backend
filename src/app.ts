369f81a7ecf6fda5b8ac797be11d1ee577cfd2a1
Rahooftv <rahooftv5885@gmail.com>
2025-12-05 02:44:42 +0530

    setup login and register

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import authRoute from "./modules/auth/auth.route"
 import taskRoute from "./modules/task/task.route"
 
@@ -11,6 +12,13 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, 
+}));
+
 
 app.use("/auth",authRoute)
 app.use("/task/", authMiddleware.protect , taskRoute)
